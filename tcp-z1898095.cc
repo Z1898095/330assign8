8564ae1953f50a5c0bdef61ffe83f7a21c3d7fc5
Brian McCarthy <Z1898095@students.niu.edu>
2020-11-20 21:54:33 -0600

    m

--- tcp-z1898095.cc
+++ tcp-z1898095.cc
@@ -14,6 +14,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <iostream>
+#include <dirent.h>
+#include <vector>
 
 int port;
 char* root_directory;
@@ -92,26 +94,93 @@ int main(int argc, char* argv[]) {
             printf("Waiting for client message:\n");
 
             // read a message from the client
-            char message_buffer[256];
+            char message[256];
             ssize_t received_len;
-            if ((received_len = read(newSock, message_buffer, 256)) < 0)
+            if ((received_len = read(newSock, message, 256)) < 0)
             {
                 perror("Failed to receive message");
                 shutdown(newSock, SHUT_RD);
                 exit(EXIT_FAILURE);
             }
 
-            printf("Client message received:\n %s\n", message_buffer);
+            printf("Client message received:\n %s\n", message);
+
+            // PARSE MESSAGE
+            /**
+             * GET /asdf                folder -> return contents of index.html from that folder if it exists
+             *                            otherwise, return a list of files in the directory
+             * GET /                    root directory
+             * GET /asdf/               trailing '/' -> can be ignored
+             * GET /asdf [other stuff]  -> ignore [other stuff] (truncate it)
+             * GET /..                 .. -> crash with error
+             * 
+             */
+
+            if (strlen(message) < 5) {
+                // message is less than 5 bytes, no way it is ever valid
+                perror("Request too short");
+                shutdown(newSock, SHUT_RD);
+                exit(EXIT_FAILURE);
+            }
+
+            // check that prefix is equal to "GET /"
+            char* msg_prefix;
+            strncpy(msg_prefix, message, 5);
+            if (strcmp("GET /", msg_prefix) != 0) {
+                // message doesn't start with "GET /" so it is malformed
+                perror("Request is malformed.");
+                shutdown(newSock, SHUT_RD);
+                exit(EXIT_FAILURE);
+            }
+
+            // Concat root directory with message directory
+            // e.g.
+            // root_directory = "serverfiles"
+            // message = "GET /dog"
+            // directory -> "serverfiles/dog"
+            char* directory;
+            strcpy(directory, root_directory);
+            strcat(directory, message[4]);
+
+            // try to open directory
+            printf("Attempting to open directory: %s\n", directory);
+            DIR* p_dir;
+            p_dir = opendir(directory);
+            if (p_dir == NULL) {
+                perror("Directory does not exist\n");
+                shutdown(newSock, SHUT_RD);
+                exit(EXIT_FAILURE);
+            }
+
+            // Loop through files in the directory. If we find one called "index.html", open it and send to client.
+            // Otherwise, add the name of each file to a message
+            printf ("Looking through files in directory:\n");
+            
+            char* return_message = "";
+            struct dirent* p_direntry;
+            while ((p_direntry = readdir(p_dir)) != NULL) {
+                printf ("[%s]\n", p_direntry->d_name);
+                if (strcmp(p_direntry->d_name, "index.html") ==) {
+                    printf ("index.html found\n");
+                }
+                strcat(return_message, " ");
+                strcat(return_message, p_direntry->d_name);
+            }
 
             // write the message back to client
-            if (write(newSock, message_buffer, received_len) != received_len)
+            if (write(newSock, return_message, sizeof(return_message)) != sizeof(return_message))
             {
-                perror("Mismatch in number of bytes");
+                perror("Bytes lost when sending message");
                 shutdown(newSock, SHUT_RD);
                 exit(EXIT_FAILURE);
             }
 
             shutdown(newSock, SHUT_RD);
+        } else if (pid == -1) {
+            perror("fork failed");
+            shutdown(newSock, SHUT_RD);
+            exit(EXIT_FAILURE);
         }
+
     }
 }
